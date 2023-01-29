4116d54bb8f4a312033895eff46726e030a6821c
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-01-29 16:15:39 +0530

    Update Charging.c

--- Charging.c
+++ Charging.c
@@ -1,11 +1,5 @@
 #include "charging.h"
 #include <stdio.h>
-
-int number_of_readings = 15;
-int count_Range1=0;
-int count_Range2=0;
-int currentReadings_a[] = {3,5,4,10,6,7,8,9,3,5,4,7,10,8,9};
-void GetRangeOfReading();
 void GetRangeOfReading(int current_a[])
 {
   int i=0;
