b52efa5f8d133b1dec3f936089a4499a19428629
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-01-29 16:34:29 +0530

    Update Charging.c

--- Charging.c
+++ Charging.c
@@ -7,20 +7,18 @@ void GetRangeOfReading(int current_a[])
   {
     if((current_a[i] >=3) && (current_a[i] <6))
     {
-        count_Range1 +=1;     
+      count_Range1 +=1;     
     }
     else
     {
-        count_Range2 +=1;
+      count_Range2 +=1;
     }
   }
   printf("\ncount_Range1=%d",count_Range1);
   printf("\ncount_Range2=%d",count_Range2);
 }
-
 int main()
 {
-    GetRangeOfReading(currentReadings_a);
-    return 0;
+  GetRangeOfReading(currentReadings_a);
+  return 0;
 }
-/*EOL*/
