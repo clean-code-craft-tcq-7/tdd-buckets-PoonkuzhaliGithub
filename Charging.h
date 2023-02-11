2e662126265a07dd30528e97757d952baf3ba6dd
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-11 18:52:00 +0530

    Update Charging.h

--- Charging.h
+++ Charging.h
@@ -5,7 +5,7 @@ int count_Range1=0;
 int count_Range2=0;
 int currentReadings_a[] = {3,5,4,10,6,7,8,9,3,5};
 
-void GetRangeOfReading();
-void GetRangeOfReading();
+void CountRange_One();
+void CountRange_Two();
 int CalculateRange1();
 int CalculateRange2();
