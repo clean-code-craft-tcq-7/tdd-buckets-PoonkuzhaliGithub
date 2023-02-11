dc0a9dfc48d3cfd01819676a173624b203b2de25
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-11 20:00:13 +0530

    Update Charging.h

--- Charging.h
+++ Charging.h
@@ -1,11 +1,10 @@
 #include "stdio.h"
 
 int count_Range1=0;
-int count_Range2=0;
+//int count_Range2=0;
 int currentReadings_a = 0;
 int num_of_readings = 0;
 
-int CountRange_One(int current_a[]);
 int CountRange_Two(int current_a[]);
-int CalculateRange1(int currentRed_a[]);
-int CalculateRange2(int currentRed_a[]);
+int CalculateRange1(int* currentRed_a[],int number_of_readings);
+//int CalculateRange2(int* currentRed_a[], int number_of_readings);
